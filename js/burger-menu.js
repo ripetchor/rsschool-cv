5053d9ef9610d2c52d8c142da38618e6a53b4b35
Jakhongir <zhaxongir.sirozhiddinov@gmail.com>
2022-12-06 00:12:07 +0500

    feat: add burger menu

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,1 +1,21 @@
 'use strict';
+
+window.onload = () => {
+  const menu = document.querySelector('.menu');
+  const burgerToggle = document.querySelector('.burger-toggle');
+  const openBurger = document.querySelector('.open-burger');
+  const closeBurger = document.querySelector('.close-burger');
+
+  burgerToggle.addEventListener('click', () => {
+    closeBurger.classList.toggle('hidden');
+    openBurger.classList.toggle('hidden');
+  });
+
+  openBurger.addEventListener('click', () => {
+    menu.style.display = 'block';
+  });
+
+  closeBurger.addEventListener('click', () => {
+    menu.style.display = 'none';
+  });
+};
